0134f22084c8d9f729edb8ae0c02ee5dfd493839
Lucas Oliveira <48152939+OliveiraLucas10@users.noreply.github.com>
2020-05-13 20:06:27 +0100

    Prototype

---
+++ Prototype/src/com/oliveiralucas/java/Demo3.java
@@ -0,0 +1,34 @@
+package com.oliveiralucas.java;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.SerializationUtils;
+
+// some libraries use reflection (no need for Serializable)
+class Foo implements Serializable {
+    public int stuff;
+    public String whatever;
+
+    public Foo(int stuff, String whatever) {
+	this.stuff = stuff;
+	this.whatever = whatever;
+    }
+
+    @Override
+    public String toString() {
+	return "Foo{" + "stuff=" + stuff + ", whatever='" + whatever + '\'' + '}';
+    }
+}
+
+class CopyThroughSerializationDemo {
+    public static void main(String[] args) {
+	Foo foo = new Foo(42, "life");
+	// use apache commons!
+	Foo foo2 = (Foo) SerializationUtils.clone(foo);
+
+	foo2.whatever = "xyz";
+
+	System.out.println(foo);
+	System.out.println(foo2);
+    }
+}
